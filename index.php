0c4f9e2be0e49f1874a4ed43468c007b919da037
dzungnv02 <dungnv02@gmail.com>
2018-04-11 20:04:36 +0700

    phpinfo

--- index.php
+++ index.php
@@ -53,6 +53,8 @@
  *
  * NOTE: If you change these, also change the error_reporting() code below
  */
+phpinfo();
+die();
 	define('ENVIRONMENT', isset($_SERVER['CI_ENV']) ? $_SERVER['CI_ENV'] : 'development');
 
 /*
