b38840331f0cfcf05c728b4d8fe7ec5d2b256523
Nguyen Vu Dung <dungnv@nal.vn>
2018-07-04 23:59:11 +0700

    add .htaccess

--- application/helpers/zoho_helper.php
+++ application/helpers/zoho_helper.php
@@ -10,7 +10,10 @@ class ZohoConnect {
 
   public function getgetGrantCode () {
     $endpoint = 'https://accounts.zoho.com/oauth/v2/auth?scope=ZohoCRM.modules.ALL&client_id=1000.A6WRD8GD1RQI14324L0PSMKXY88TRU&response_type=code&access_type=online&redirect_uri=https://zohoconnect.herokuapp.com/zohoconnected';
-    $responseBody = $this->client->request($endpoint, 'GET','json');
+    $option = [
+      'http_errors' => true
+    ];
+    $responseBody = $this->client->request('GET',$endpoint,$option);
     return $responseBody;
   }
 }
